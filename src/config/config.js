4f0ae4b993e7d43e852e2de4a63f309bf9658b5d
Alunos <Alunos>
2018-11-27 16:48:11 -0300

    Backup

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 let config = {
     local: "http://localhost:54987",
-    host: "https://api-football-fssnyxxunr.now.sh"
+    host: "https://api-football-5hv60iqq1.now.sh"
 }
 
 module.exports = config;
