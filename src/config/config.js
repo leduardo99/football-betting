62ab599ed4443dbb55b4fa0821b4609ebd18d125
Alunos <Alunos>
2018-11-27 17:36:47 -0300

    Backup

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 let config = {
     local: "http://localhost:54987",
-    host: "https://api-football-5hv60iqq1.now.sh"
+    zeit: "https://api-football-5hv60iqq1.now.sh",
+    openode: "http://teste-api.us.openode.io/"
 }
 
 module.exports = config;
